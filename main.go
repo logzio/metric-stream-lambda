75f57bbe5953a6475e92a6f2dddea7fdca1cb09d
yotamloe <yotam12341@gmail.com>
2022-04-13 16:29:33 +0300

    calculate values (sum, count, min, max) from summary metric

--- main.go
+++ main.go
@@ -22,6 +22,13 @@ import (
 	"log"
 )
 
+const (
+	sumStr   = "_sum"
+	countStr = "_count"
+	minStr   = "_min"
+	maxStr   = "_max"
+)
+
 // Takes a base64 encoded string and returns decoded string
 func base64Decode(str string) (string, bool) {
 	data, err := base64.StdEncoding.DecodeString(str)
@@ -30,6 +37,74 @@ func base64Decode(str string) (string, bool) {
 	}
 	return string(data), false
 }
+
+// Takes origin metric and suffix based on desired aggregation. Creates new DoubleSum metric with origin name plus suffix, origin unit and origin description
+func createMetricFromAttributes(metric pdata.Metric, suffix string) pdata.Metric {
+	destMetric := pdata.NewMetric()
+	destMetric.SetName(metric.Name() + suffix)
+	destMetric.SetDataType(pdata.MetricDataTypeDoubleSum)
+	destMetric.SetUnit(metric.Unit())
+	destMetric.SetDescription(metric.Description())
+	destMetric.DoubleSum().SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+	return destMetric
+}
+
+// Takes origin datapoint, destination datapoint and resource attributes. Coping labels from origin and adding resource attributes as labels
+func addLabelsAndResourceAttributes(dp pdata.SummaryDataPoint, destDp pdata.DoubleDataPoint, resourceAttributes pdata.AttributeMap) {
+	dp.LabelsMap().Range(func(k string, v string) bool {
+		destDp.LabelsMap().Insert(strings.ToLower(k), strings.ToLower(v))
+		return true
+	})
+	accountId, _ := resourceAttributes.Get("cloud.account.id")
+	region, _ := resourceAttributes.Get("cloud.region")
+	destDp.LabelsMap().Insert("account", accountId.StringVal())
+	destDp.LabelsMap().Insert("region", region.StringVal())
+}
+
+// Takes Summary metric and generates new metrics (sum, count, min, max) than add them to metricsToSend
+func summaryValuesToMetrics(metricsToSendSlice pdata.InstrumentationLibraryMetricsSlice, metric pdata.Metric, resourceAttributes pdata.AttributeMap) {
+	// Converts to new name (example: amazonaws.com/AWS/AppRunner/ActiveInstances -> aws_apprunner_activeinstances)
+	newName := strings.ReplaceAll(strings.ToLower(strings.ReplaceAll(metric.Name(), "/", "_")), "amazonaws.com_", "")
+	metric.SetName(newName)
+	// Assuming Summary metric type according to AWS docs:
+	// https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/acw-ug.pdf#CloudWatch-metric-streams-formats-opentelemetry
+	// Page 262
+	var dps = metric.Summary().DataPoints()
+	// Generate new metrics (sum, count, min, max), and for each summary data point, lowercase all attributes and add resource attributes
+	sumMetric := createMetricFromAttributes(metric, sumStr)
+	countMetric := createMetricFromAttributes(metric, countStr)
+	maxMetric := createMetricFromAttributes(metric, maxStr)
+	minMetric := createMetricFromAttributes(metric, minStr)
+	for d := 0; d < dps.Len(); d++ {
+		datapoint := dps.At(d)
+		// Sum datapoint
+		sumDp := sumMetric.DoubleSum().DataPoints().AppendEmpty()
+		sumDp.SetValue(datapoint.Sum())
+		sumDp.SetTimestamp(datapoint.Timestamp())
+		addLabelsAndResourceAttributes(datapoint, sumDp, resourceAttributes)
+		// Count datapoint
+		countDp := countMetric.DoubleSum().DataPoints().AppendEmpty()
+		countDp.SetValue(float64(datapoint.Count()))
+		countDp.SetTimestamp(datapoint.Timestamp())
+		addLabelsAndResourceAttributes(datapoint, countDp, resourceAttributes)
+		// Max datapoint
+		minDp := maxMetric.DoubleSum().DataPoints().AppendEmpty()
+		minDp.SetValue(datapoint.QuantileValues().At(0).Value())
+		minDp.SetTimestamp(datapoint.Timestamp())
+		addLabelsAndResourceAttributes(datapoint, minDp, resourceAttributes)
+		// Min datapoint
+		maxDp := minMetric.DoubleSum().DataPoints().AppendEmpty()
+		maxDp.SetValue(datapoint.QuantileValues().At(1).Value())
+		maxDp.SetTimestamp(datapoint.Timestamp())
+		addLabelsAndResourceAttributes(datapoint, maxDp, resourceAttributes)
+	}
+	// Add new aggregated metrics to destination
+	sumMetric.CopyTo(metricsToSendSlice.AppendEmpty().Metrics().AppendEmpty())
+	countMetric.CopyTo(metricsToSendSlice.AppendEmpty().Metrics().AppendEmpty())
+	maxMetric.CopyTo(metricsToSendSlice.AppendEmpty().Metrics().AppendEmpty())
+	minMetric.CopyTo(metricsToSendSlice.AppendEmpty().Metrics().AppendEmpty())
+}
+
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println("Getting secretes from header")
 	metricCount := 0
@@ -63,7 +138,7 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 		"Authorization": fmt.Sprintf("Bearer %s", LOGZIO_TOKEN),
 	}
 	cfg.Namespace = ""
-	cfg.ExternalLabels = map[string]string{"p8s_logzio_name": "cloudwatch-stream-metrics"}
+	cfg.ExternalLabels = map[string]string{"p8s_logzio_name": "otlp-cloudwatch-stream-metrics"}
 	cfg.RemoteWriteQueue.NumConsumers = 1
 	log.Println("Starting metrics exporter")
 	metricsExporter, err := prometheusremotewriteexporter.NewPRWExporter(cfg, buildInfo)
@@ -102,45 +177,30 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 		}
 		// Converting otlp proto bytes to pdata.metrics
 		metrics, err := pdata.MetricsFromOtlpProtoBytes(protoBytes)
-		metricCount += metrics.MetricCount()
-		// Lopping threw all data points and enhance labels with logz.io naming conventions and resource attributes
+		// represents the metrics to send after converting from summary to sum, count, min, max (those are the metrics we are actually going to send)
+		metricsToSend := pdata.NewMetrics()
+		aggregatedInstrumentationLibraryMetrics := metricsToSend.ResourceMetrics().AppendEmpty().InstrumentationLibraryMetrics()
+		// Lopping threw all metrics and data points, generate new metrics (sum, count, min, max) and enhance labels with logz.io naming conventions and resource attributes
 		for i := 0; i < metrics.ResourceMetrics().Len(); i++ {
 			resource := metrics.ResourceMetrics().At(i)
 			resourceAttributes := resource.Resource().Attributes()
 			for j := 0; j < resource.InstrumentationLibraryMetrics().Len(); j++ {
-				metricsList := resource.InstrumentationLibraryMetrics().At(j)
-				for n := 0; n < metricsList.Metrics().Len(); n++ {
-					metric := metricsList.Metrics().At(n)
-					// Converts to new name (example: amazonaws.com/AWS/AppRunner/ActiveInstances -> aws_apprunner_activeinstances)
-					newName := strings.ReplaceAll(strings.ToLower(strings.ReplaceAll(metric.Name(), "/", "_")), "amazonaws.com_", "")
-					metric.SetName(newName)
-					// Assuming Summary metric type according to AWS docs:
-					// https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/acw-ug.pdf#CloudWatch-metric-streams-formats-opentelemetry
-					// Page 262
-					var dps = metric.Summary().DataPoints()
-					for d := 0; d < dps.Len(); d++ {
-						// For each data point, lowercase all attributes and add resource attributes
-						datapoint := dps.At(d)
-						datapoint.LabelsMap().Range(func(k string, v string) bool {
-							datapoint.LabelsMap().Insert(strings.ToLower(k), strings.ToLower(v))
-							datapoint.LabelsMap().Delete(k)
-							return true
-						})
-						accountId, _ := resourceAttributes.Get("cloud.account.id")
-						region, _ := resourceAttributes.Get("cloud.region")
-						datapoint.LabelsMap().Insert("account", accountId.StringVal())
-						datapoint.LabelsMap().Insert("region", region.StringVal())
-					}
+				instrumentationLibraryMetrics := resource.InstrumentationLibraryMetrics().At(j)
+				for k := 0; k < instrumentationLibraryMetrics.Metrics().Len(); k++ {
+					metric := instrumentationLibraryMetrics.Metrics().At(k)
+					summaryValuesToMetrics(aggregatedInstrumentationLibraryMetrics, metric, resourceAttributes)
 				}
 			}
 		}
-		log.Println(fmt.Sprintf("Found %d new metircs", metrics.MetricCount()))
+		numberOfMetrics, numberOfDataPoints := metricsToSend.MetricAndDataPointCount()
+		log.Println(fmt.Sprintf("Found %d new metircs with %d datapoints", numberOfMetrics, numberOfDataPoints))
 		if err != nil {
 			log.Printf("Error: %s", err)
 			return events.APIGatewayProxyResponse{Body: "Error", StatusCode: 500}, err
 		}
+		metricCount += numberOfMetrics
 		log.Println("Sending metrics")
-		err = metricsExporter.PushMetrics(ctx, metrics)
+		err = metricsExporter.PushMetrics(ctx, metricsToSend)
 		if err != nil {
 			log.Printf("Error while pushing metrics: %s", err)
 			return events.APIGatewayProxyResponse{Body: "Error while pushing metrics", StatusCode: 500}, err
