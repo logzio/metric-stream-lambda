0399865408b7d212e6417d406ea185983f18b609
Yotam loewenbach <yotam.loewenbach@logz.io>
2022-12-18 12:50:12 +0200

    Remove `oldlog` + remove set timestamp for s3

--- handler/handler.go
+++ handler/handler.go
@@ -17,7 +17,6 @@ import (
 	"go.opentelemetry.io/collector/exporter/prometheusremotewriteexporter"
 	pb "go.opentelemetry.io/proto/otlp/collector/metrics/v1"
 	"go.uber.org/zap"
-	logold "log"
 	"os"
 	"strings"
 	"time"
@@ -109,7 +108,8 @@ func initLogger(ctx context.Context, request events.APIGatewayProxyRequest) zap.
 	}
 	logger, configErr := config.Build()
 	if configErr != nil {
-		logold.Fatal(configErr)
+		fmt.Printf("Error while initializing the logger: %v", configErr)
+		panic(configErr)
 	}
 	return *logger.Sugar()
 }
@@ -207,53 +207,33 @@ func summaryValuesToMetrics(metricsToSendSlice pdata.InstrumentationLibraryMetri
 		datapoint := dps.At(d)
 		namespace, _ := datapoint.LabelsMap().Get("Namespace")
 		detectedNamespaces = append(detectedNamespaces, namespace)
-		setTimestampNow := namespace == "AWS/S3"
 		// Sum datapoint
 		sumDp := sumMetric.DoubleSum().DataPoints().AppendEmpty()
 		sumDp.SetValue(datapoint.Sum())
-		if setTimestampNow {
-			sumDp.SetTimestamp(pdata.TimestampFromTime(time.Now()))
-		} else {
-			sumDp.SetTimestamp(datapoint.Timestamp())
-		}
+		sumDp.SetTimestamp(datapoint.Timestamp())
 		addLabelsAndResourceAttributes(datapoint, sumDp, resourceAttributes)
 		// Count datapoint
 		countDp := countMetric.DoubleSum().DataPoints().AppendEmpty()
 		countDp.SetValue(float64(datapoint.Count()))
-		if setTimestampNow {
-			countDp.SetTimestamp(pdata.TimestampFromTime(time.Now()))
-		} else {
-			countDp.SetTimestamp(datapoint.Timestamp())
-		}
+
+		countDp.SetTimestamp(datapoint.Timestamp())
 		addLabelsAndResourceAttributes(datapoint, countDp, resourceAttributes)
 		// Min datapoint
 		minDp := minMetric.DoubleSum().DataPoints().AppendEmpty()
 		minDp.SetValue(datapoint.QuantileValues().At(0).Value())
-		if setTimestampNow {
-			minDp.SetTimestamp(pdata.TimestampFromTime(time.Now()))
-		} else {
-			minDp.SetTimestamp(datapoint.Timestamp())
-		}
+		minDp.SetTimestamp(datapoint.Timestamp())
 		addLabelsAndResourceAttributes(datapoint, minDp, resourceAttributes)
 		// Max datapoint
 		maxDp := maxMetric.DoubleSum().DataPoints().AppendEmpty()
 		maxDp.SetValue(datapoint.QuantileValues().At(datapoint.QuantileValues().Len() - 1).Value())
-		if setTimestampNow {
-			maxDp.SetTimestamp(pdata.TimestampFromTime(time.Now()))
-		} else {
-			maxDp.SetTimestamp(datapoint.Timestamp())
-		}
+		maxDp.SetTimestamp(datapoint.Timestamp())
 		addLabelsAndResourceAttributes(datapoint, maxDp, resourceAttributes)
 		// If the count value is greater than 1, and we have more than 2 Quantiles we need to add datapoints for each quantileValues
 		if datapoint.Count() > 1 && datapoint.QuantileValues().Len() > 2 && datapoint.Sum() > 0 {
 			for i := 1; i < datapoint.QuantileValues().Len()-1; i++ {
 				quantileDp := quantileMetric.DoubleSum().DataPoints().AppendEmpty()
 				quantileDp.SetValue(datapoint.QuantileValues().At(i).Value())
-				if setTimestampNow {
-					quantileDp.SetTimestamp(pdata.TimestampFromTime(time.Now()))
-				} else {
-					quantileDp.SetTimestamp(datapoint.Timestamp())
-				}
+				quantileDp.SetTimestamp(datapoint.Timestamp())
 				quantileDp.LabelsMap().Insert("quantile", fmt.Sprintf("%v", datapoint.QuantileValues().At(i).Quantile()))
 				addLabelsAndResourceAttributes(datapoint, quantileDp, resourceAttributes)
 			}
