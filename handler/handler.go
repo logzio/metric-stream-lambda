581999cdc7de4033eb252f740783a917bfc7bd94
Yotam loewenbach <yotam.loewenbach@logz.io>
2022-12-19 14:11:34 +0200

    flush buffered logs if exists, before the function run ends

--- handler/handler.go
+++ handler/handler.go
@@ -250,6 +250,14 @@ func summaryValuesToMetrics(metricsToSendSlice pdata.InstrumentationLibraryMetri
 }
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log := initLogger(ctx, request)
+	// flush buffered logs if exists, before the function run ends
+	defer func(log *zap.SugaredLogger) {
+		log.Infof("Syncing logger and finishing function run")
+		err := log.Sync()
+		if err != nil {
+			log.Errorf("Error while logger sync: %v", err)
+		}
+	}(&log)
 	metricCount := 0
 	dataPointCount := 0
 	shippingErrors := new(ErrorCollector)
